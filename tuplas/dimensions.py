7fa081edb9f23461840fe2108b111bcaf6dad197
Beatriz Almeida <beatrizalmeida.ufg@gmail.com>
2025-01-19 12:41:51 -0300

    update

--- tuplas/dimensions.py
+++ tuplas/dimensions.py
@@ -1,7 +1,7 @@
 dimensions = (200,50)
 print(dimensions[0])
 
-# dimensions[0] = 250 -> isso msotra que é imutavel
+# dimensions[0] = 250 -> isso mostra que é imutavel
 
 # mas é possivel sobrescrever uma tupla
 
